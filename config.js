03dee6467bcb2033e38bcef9193d8157abc0594c
JairHerrera <jair.herrera@gmail.com>
2025-06-30 12:27:18 -0600

    Actualiza config.js para usar URL de producción en Railway

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js
+//URL DE SERVIDOR
 export const API_URL = __DEV__
-  ? 'http://192.168.100.4:3000'               // Desarrollo local
-  : 'https://mi-panic-button.up.railway.app'; // Producción en Railway
+  ? 'http://192.168.100.4:3000' // tu entorno local
+  : 'https://api-boton-uteq-production.up.railway.app'; // URL en Railway
